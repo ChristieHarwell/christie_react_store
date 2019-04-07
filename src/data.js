d27341cc2045ffce2518d2693f9c3b549463f2c3
Christie Williams <christiewilliams@Christie-Williamss-iMac.local>
2019-04-06 22:20:24 -0400

    added data.js file of product information.

---
+++ src/data.js
@@ -0,0 +1,49 @@
+
+    {
+        "product":{
+            "name":"LRho for men",
+            "details":"details for LRhodesia for men details",
+            "notes":"musk",
+            "price":15
+        }            
+    },
+    {
+        "product":{
+            "name":"LRho for women",
+            "details":"details for LRhodesia for women details",
+            "notes":"amber",
+            "price":15
+        }
+    },
+    {
+        "product":{
+            "name":"LRhodesia for unisex",
+            "details":" details for LRhodesia for unisex details",
+            "notes":"patchouli",
+            "price":15
+        }
+    }
+    {
+        "product":{
+            "name":"LRho sport",
+            "details":" details for LRhod sport for men details",
+            "notes":["sandalwood","musk"],
+            "price":15
+        }
+    },
+    {
+        "product":{
+            "name":"Rhomance for women",
+            "details":" details for Rhomance for women details",
+            "notes":["amber","vanilla","patchouli"],
+            "price":15
+        }
+    },
+    {
+        "product":{
+            "name":"Rhomance for men",
+            "details":" details for Rhomance for men details",
+            "notes":"sandalwood",
+            "price":15
+        }
+    }
