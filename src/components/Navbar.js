9bffec9ead3cdd7a74600481e2387baa6ff4a9be
Christie Williams <christiewilliams@Christie-Williamss-iMac.local>
2019-04-06 23:35:52 -0400

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { NavDropdown } from 'react-bootstrap';
+
 
 export default class Navbar extends Component {
   render() {
