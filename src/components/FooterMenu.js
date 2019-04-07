d3a44a7bbffba9f709c8cf216a02d6c148705fe9
Christie Williams <christiewilliams@Christie-Williamss-iMac.local>
2019-04-06 20:37:28 -0400

    first commit after creating react app initial dependencies and files installed

---
+++ src/components/FooterMenu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FooterMenu extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello from footerMenu</h3>
+      </div>
+    )
+  }
+}
